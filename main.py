7d1c5b4e5afce9c25c065584bb0048e4c01f9b66
galmelman <165789349+galmelman@users.noreply.github.com>
2024-09-06 18:40:16 +0300

    Implement SQLite database and refactor application structure
    - Add SQLite database integration for persistent data storage
    - Create new database.py file for handling database operations
    - Update models.py to include ID attribute in JobApplication class
    - Refactor controllers.py to work with new database structure
    - Modify views.py to use updated controller functions and database operations
    - Implement default date functionality when adding new applications
    - Update Treeview in ApplicationTracker to display application ID
    - Improve error handling and input validation in add_or_update_application function
    - Optimize data retrieval and update processes for better performance
    - Enhance code modularity and maintainability across all components

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from views import ApplicationTracker, create_analytics_frame
+from views import ApplicationTracker
 from utils import setup_reminder_check
 
 if __name__ == "__main__":
@@ -8,8 +8,5 @@ if __name__ == "__main__":
 
     # Start the reminder check process
     setup_reminder_check(root, app.applications)
-
-    # Add Analytics Frame
-    analytics_frame = create_analytics_frame(root, app.applications)
 
     root.mainloop()
